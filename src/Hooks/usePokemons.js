1de7a2500ff7d881308b498a9252adac8b409826
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-12-06 23:38:26 -0300

    Se agrega una pantalla de carga
    Esta se agrega para ser mostrada mientra se espera que cargen los datos solicitados

--- src/Hooks/usePokemons.js
+++ src/Hooks/usePokemons.js
@@ -8,43 +8,59 @@ const usePokemons = () => {
     const { pokemons, setPokemons } = useContext(Context)
     const [nextPage, setNextPage] = useState('')
     const [previousPage, setPreviousPage] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
     // const [indice, setIndice] = useState(0)
     // const [listValue, setListValue] = useLocalStorage({ itemName: `pokemons_${indice}`, initialValue: [] })
 
     useEffect(() => {
+        setIsLoading(true)
         getPokemons({})
             .then(({ pokemons, next, previous }) => {
                 setPokemons(pokemons)
                 setNextPage(next)
                 setPreviousPage(previous)
+                setIsLoading(false)
                 // setIndice(indice + 1)
                 // setListValue(pokemons)
+            })
+            .catch(error => {
+                setIsLoading(false)
             })
 
 
     }, [setPokemons])
 
     const getNextPage = (() => {
+        setIsLoading(true)
         getPokemons({ url: nextPage })
             .then(({ pokemons, next, previous }) => {
                 setPokemons(pokemons)
                 setNextPage(next)
                 setPreviousPage(previous)
+                setIsLoading(false)
                 // setIndice(indice + 1)
                 // setListValue(pokemons)
+            })
+            .catch(error => {
+                setIsLoading(false)
             })
 
     })
 
     const getPreviousPage = (() => {
+        setIsLoading(true)
         getPokemons({ url: previousPage })
             .then(({ pokemons, next, previous }) => {
                 setPokemons(pokemons)
                 setNextPage(next)
                 setPreviousPage(previous)
+                setIsLoading(false)
+            })
+            .catch(error => {
+                setIsLoading(false)
             })
     })
-    return { pokemons, getNextPage, getPreviousPage }
+    return { pokemons, getNextPage, getPreviousPage, isLoading }
 }
 
 export default usePokemons
--- src/Hooks/useSinglePokemon.js
+++ src/Hooks/useSinglePokemon.js
@@ -3,14 +3,21 @@ import { useEffect, useState } from "react"
 
 const useSinglePokemon = ({ keyword }) => {
     const [singlePokemon, setSinglePokemon] = useState()
+    const [isLoading, setIsLoading] = useState(false)
     useEffect(() => {
+        
         if (!singlePokemon) {
+            setIsLoading(true)
             getSinglePokemon({ keyword })
                 .then(pokemon => {
                     setSinglePokemon(pokemon)
+                    setIsLoading(false)
+                })
+                .catch(error => {
+                    setIsLoading(false)
                 })
         }
     }, [singlePokemon, keyword])
-    return { singlePokemon }
+    return { singlePokemon, isLoading }
 }
 export default useSinglePokemon
